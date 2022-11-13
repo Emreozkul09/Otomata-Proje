720bd37327f0aff5ffb58dcc0895bc0c0bc82a3f
Emre Özkul <ozkul9018@gmail.com>
2022-11-13 15:09:31 +0300

    first commit

---
+++ otomata/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace otomata
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(194, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "alfabe harflerini giriniz lütfen";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(194, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "düzenli ifedeyi giribiz lütfen";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(194, 165);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(172, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "istenilen kelime sayısını giriniz lütfen";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(503, 70);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(503, 115);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(503, 165);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(625, 70);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Üret";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(625, 112);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Temizle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(197, 228);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(262, 95);
+            this.listBox1.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(707, 70);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Alfabeniz";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(706, 99);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(78, 95);
+            this.listBox2.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(529, 228);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Arama";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(532, 253);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 12;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(532, 279);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Listede ara";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(532, 319);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "uyumlu mu";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ otomata/Form1.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace otomata
+{
+    public partial class Form1 : Form
+    {
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string[] harfler = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "c", "j", "k", "l", "m", "n", "o", "p", "s", "t", "u", "v", "y", "z" };
+        int[] sayilar = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        string virgul;
+        static string alfabe;
+        static string duzenlimetin;
+        static int konum2;
+        int kelimeSayisi = 0;
+        string[] bolunmusMetin;
+        string[] bolunmusMetin2;
+        string dizi2 = " ";
+        int sayac = 0;
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            virgul = ",";
+            alfabe = textBox1.Text;
+            int konum = alfabe.IndexOf(virgul);
+
+            if (konum == -1)
+            {
+                MessageBox.Show("Oluşturulan Alfabede , bulunmamaktadır.Lütfen her harften sonra kulanın.");
+
+            }
+            duzenlimetin = textBox2.Text;
+            int acikParantezIndex = duzenlimetin.IndexOf("(");
+            int kapaliParantezKonum = duzenlimetin.IndexOf(")");
+            int yildizindex = duzenlimetin.IndexOf("*");
+            int artiindex = duzenlimetin.IndexOf("+");
+           
+            char[] karakterAyirma = duzenlimetin.ToCharArray();
+            char yildiz = '*';
+            int sayac = 1;
+            string dizi = "";
+            string pdizi = "";
+            string letter = "";
+            string altsab = "";
+            string Artisab = "";
+            string yenisab = "";
+            int a = 1;
+            kelimeSayisi = Convert.ToInt32(textBox3.Text);
+            
+            
+            //MessageBox.Show(Convert.ToString(acikParantezIndex));
+            // MessageBox.Show(Convert.ToString(kapaliParantezKonum));
+
+            for (int m = 0; m < kelimeSayisi; m++)
+            {
+
+                var random = new Random();
+                int abcd = random.Next(0, kelimeSayisi);
+
+                for (int i = 0; i < karakterAyirma.Length; i++)
+                   
+                {
+                    
+                    if (karakterAyirma[i] == yildiz)
+                    {
+                        
+                        for (int j = 1; j < a; j++)
+                        {
+                            
+                            if (karakterAyirma[i - 1] == ')')
+                            {
+                                continue;
+                            }
+                            
+                            else
+                            {
+                                dizi = dizi + karakterAyirma[i - 1];
+                            }
+
+                        }
+
+
+                    }
+
+                    else if (karakterAyirma[i] == '(')
+
+                    {
+
+                        if (kapaliParantezKonum + 1 == yildizindex)
+                        {
+                            for (int j = 0; j <a; j++)
+                            {
+                                //dizi = dizi + karakterAyirma[i - 1];
+
+                                pdizi = duzenlimetin.Substring((acikParantezIndex + 1), (kapaliParantezKonum - 2));
+                                string[] pdizi2 = pdizi.Split('+');
+                                Random rd = new Random();
+                                int abc=rd.Next(0, 2);
+                                dizi = dizi + pdizi2[abc];
+                                i = kapaliParantezKonum + 1;
+                                
+                            }
+                        }
+                        else
+                        {
+                            // dizi = dizi + duzenlimetin.Substring((acikParantezIndex + 1), (kapaliParantezKonum - 2));
+                            continue;
+
+                        }
+
+
+
+                        /*if (karakterAyirma[kapaliParantezKonum + 1] == yildiz)
+                        {
+                            int alinacakKarakterSayisi = (kapaliParantezKonum - (acikParantezIndex + 1));
+                            MessageBox.Show("deger" + alinacakKarakterSayisi);
+                            string kesilenDeger = duzenlimetin.Substring((acikParantezIndex + 1), alinacakKarakterSayisi);
+                            dizi = dizi + kesilenDeger;
+                            //   dizi = dizi + duzenlimetin.Substring((acikParantezIndex + 1), (kapaliParantezKonum - 2));
+                            // dizi =dizi+karakterAyirma[acikParantezIndex + sayac];
+
+                            if (karakterAyirma[acikParantezIndex + sayac] == ')')
+                            {
+                                break;
+                            }
+                            sayac++;
+                            MessageBox.Show("giriyor işlem");
+                        }
+                        */
+
+                    }
+                    else if (karakterAyirma[i] == ')')
+                    {
+                        continue;
+                    }
+                    else if (karakterAyirma[i] == '+')
+                    {
+                        //dizi = dizi + karakterAyirma[i-1];
+
+                        i = artiindex + i;
+                        
+
+
+
+                    }
+                    else
+                    {
+
+
+                        if (karakterAyirma[i] == ')')
+                        {
+                            continue;
+                        }
+                        else if (karakterAyirma[i]=='+')
+                        {
+                            continue;
+                        }
+                        
+
+                        else
+                        {
+    
+                            dizi = dizi + karakterAyirma[i] ;
+  
+                        }
+
+                    }
+
+
+                }
+                dizi = dizi + " ";
+
+                a++;
+
+
+            }
+
+            
+            for (int t = 0; t < dizi.Length; t++)
+            {
+                if (dizi[t] == ')')
+                {
+                    dizi2 = dizi.Replace(')', ' ');
+                }
+                
+                else
+                {
+                    dizi2 = dizi;
+                }
+                
+
+            }
+            string[] value1 = dizi2.Split(' ');
+            foreach (string item in value1)
+            {
+                listBox1.Items.Add(item);
+            }
+            
+            string[] value2 = alfabe.Split(',');
+            foreach (string item in value2)
+            {
+                listBox2.Items.Add(item);
+            }
+
+            
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+
+        }
+
+
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {/*
+            string[] veriler1 = new string[listBox1.Items.Count];
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                veriler1[i] = listBox1.Items[i].ToString();
+            }
+
+            List<string> veriler2 = new List<string>();
+            foreach (var item in listBox1.Items)
+            {
+                veriler2.Add(item.ToString());
+            }
+
+            var veriler3 = listBox1.Items.Cast<String>().ToArray();
+            MessageBox.Show(veriler3[1]);
+            */
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                //MessageBox.Show(listBox1.Items[i].ToString());
+                if (listBox1.Items[i].ToString().Contains(textBox4.Text))
+                {
+                    listBox1.SetSelected(i, true);
+                    sayac = sayac + 1;
+                    break;
+
+
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            if (sayac >= 1)
+            {
+                MessageBox.Show("aradıgınız kelime bunmaktadır");
+                
+            }
+            else
+            {
+                MessageBox.Show("bulunmamaktadır");
+            }
+            sayac = 0;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
